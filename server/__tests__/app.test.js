a9844cccdfce6d232f48853b605797d4b862690a
Arpit Bansal <hi@arpitbansal.com>
2020-05-19 13:59:12 +0800

    Updating code quality issues

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -3,8 +3,7 @@ import app from "../app";
 const request = supertest(app);
 
 it("Node.js server should be up and running", async (done) => {
-  const response = await request.get("/")
-
+  const response = await request.get("/");
   expect(response.status).toBe(200);
   done();
 });
